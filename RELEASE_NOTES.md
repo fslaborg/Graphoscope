feb8115c54d35741b245307781c2cb728d059b20
Kevin Schneider <schneider.kev@outlook.de>
2023-06-06 16:18:19 +0200

    Add project skeleton and build logic

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,4 @@
+### 0.0.0 (Released 2023-6-6)
+* Additions:
+    * Initial set up for RELEASE_Notes.md
+
---
+++ src/Graphoscope/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Graphoscope
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
