8f13a158fe4685f3597c9f3410665eca947cf4f7
muehlhaus <timo.muehlhaus@rptu.de>
2023-07-24 09:52:49 +0200

    Init adjacency matrix storing additional node information

--- src/Graphoscope/AdjMatrix.fs
+++ src/Graphoscope/AdjMatrix.fs
@@ -4,31 +4,33 @@ open FSharpAux
 open System.Collections.Generic
 
 
+/// <summary> 
+/// A adjacency matrix storing additional node information 
+/// </summary>
+type AdjMatrix<'NodeKey, 'NodeData, 'EdgeData when 'NodeKey : comparison>
+    (edgeData : 'EdgeData array2d, nodeData : array<'NodeData>, nodekeyIndex : Dictionary<'NodeKey, int>) =
 
-/// A FsSparseMatrix
-type AdjMatrix<'T>(sparseValues : 'T array, sparseRowOffsets : int array, ncols:int, columnValues: int array) =     
-    member m.NumCols = ncols
-    member m.NumRows = sparseRowOffsets.Length - 1
-    member m.SparseColumnValues = columnValues
-    member m.SparseRowOffsets =  sparseRowOffsets (* nrows + 1 elements *)
-    member m.SparseValues =  sparseValues
+    new (nodeCount : int) = 
+        let n = nodeCount - 1
+        let nodeData     = Array.zeroCreate n
+        let nodekeyIndex = Dictionary<'NodeKey, int>(n)
+        let adjMatrix    = Array2D.zeroCreate n n        
+        AdjMatrix(adjMatrix, nodeData, nodekeyIndex)
 
-    member m.MinIndexForRow i = m.SparseRowOffsets.[i]
-    member m.MaxIndexForRow i = m.SparseRowOffsets.[i+1]
-            
+    /// <summary> 
+    /// Traverses nodes reachable from given node in a Breadth-First Traversal (or Search)
+    /// </summary>
+    /// <param name="starting">Nodekey for starting the BFS traversal.</param> 
+    /// <param name="graph">The graph to traverse.</param> 
+    /// <returns>Sequence of node key and adjacent components</returns>
+    member this.Item(n:int, n:int) = 
+         edgeData.[n,m]
+    member this.Bykey(sourceKey:'NodeKey, targetKey:'NodeKey) =
+        edgeData.[nodekeyIndex.[sourceKey], nodekeyIndex.[targetKey]]    
+    
+    member this.Nodes = nodeData
+    
+    member this.NodesByKey(key:'NodeKey) = nodeData.[nodekeyIndex.[key]]
 
-    member m.Item 
-        with get (i,j) = 
-            let imax = m.NumRows
-            let jmax = m.NumCols
-            if j < 0 || j >= jmax || i < 0 || i >= imax then raise (new System.ArgumentOutOfRangeException()) else
-            let kmin = m.MinIndexForRow i
-            let kmax = m.MaxIndexForRow i
-            let rec loopRow k =
-                (* note: could do a binary chop here *)
-                if k >= kmax then defaultEmptyValue else
-                let j2 = columnValues.[k]
-                if j < j2 then defaultEmptyValue else
-                if j = j2 then sparseValues.[k] else 
-                loopRow (k+1)
-            loopRow kmin
+    // member this.AddElement (sourceKey : 'NodeKey) (source : 'NodeData)  (targetKey : 'NodeKey) (target : 'NodeData) (data : 'EdgeData) =
+    //     nodekeyIndex
