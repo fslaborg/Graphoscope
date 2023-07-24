8f967d90090c97460c29152a9b95981c97fd4a7b
muehlhaus <timo.muehlhaus@rptu.de>
2023-07-24 14:43:08 +0200

    Update FGraph according to module structure

--- src/Graphoscope/FGraph.fs
+++ src/Graphoscope/FGraph.fs
@@ -19,46 +19,46 @@ module FGraph =
     let create<'NodeKey, 'NodeData, 'EdgeData when 'NodeKey: comparison>() : FGraph<'NodeKey, 'NodeData, 'EdgeData> =
         Dictionary<_,_>()
 
-    module Nodes =
+    type Node() =
     
         ///Adds a labeled node to the graph.
-        let add (nk:'NodeKey) (nd : 'NodeData)  (g : FGraph<'NodeKey, 'NodeData, 'EdgeData>) : FGraph<'NodeKey, 'NodeData, 'EdgeData> =
+        static member  add (nk:'NodeKey) (nd : 'NodeData)  (g : FGraph<'NodeKey, 'NodeData, 'EdgeData>) : FGraph<'NodeKey, 'NodeData, 'EdgeData> =
             Dictionary.addOrUpdateInPlace nk (Dictionary<_,_>(),nd,Dictionary<_,_>()) g |> ignore
             g
 
 
         ///Evaluates the number of nodes in the graph.
-        let count (g : FGraph<'NodeKey, 'NodeData, 'EdgeData>) : int = 
+        static member count (g : FGraph<'NodeKey, 'NodeData, 'EdgeData>) : int = 
             g.Count
  
         ///Returns true, if the node v is contained in the graph. Otherwise, it returns false.
-        let contains vk (g : FGraph<'NodeKey, 'NodeData, 'EdgeData>) : bool =
+        static member contains vk (g : FGraph<'NodeKey, 'NodeData, 'EdgeData>) : bool =
             Dictionary.containsKey vk g
 
         ///Lookup a labeled vertex in the graph. Raising KeyNotFoundException if no binding exists in the graph.
-        let find (n: 'NodeKey) (g : FGraph<'NodeKey, 'NodeData, 'EdgeData>) : ('NodeKey * 'NodeData) = 
+        static member find (n: 'NodeKey) (g : FGraph<'NodeKey, 'NodeData, 'EdgeData>) : ('NodeKey * 'NodeData) = 
             Dictionary.item n g
             |> fun (_, nd, _) -> n, nd
     
 
 
     ///Functions for edges of directed Graphs
-    module Edges = 
+    type Edge() = 
 
         ///Returns true, if the edge from vertex v1 to vertex v2 is contained in the graph. Otherwise, it returns false.
-        let contains v1 v2 (g: FGraph<'NodeKey,'NodeData,'EdgeData>) : bool =
+        static member contains v1 v2 (g: FGraph<'NodeKey,'NodeData,'EdgeData>) : bool =
             Dictionary.tryFind v1 g
             |> Option.bind (fun (_, _, s) -> Dictionary.tryFind v2 s)
             |> Option.isSome
     
         ///Lookup a labeled edge in the graph. Raising KeyNotFoundException if no binding exists in the graph.
-        let find (v1:'NodeKey) (v2:'NodeKey) (g : FGraph<'NodeKey,'NodeData,'EdgeData>) : 'NodeKey * 'NodeKey * 'EdgeData =
+        static member find (v1:'NodeKey) (v2:'NodeKey) (g : FGraph<'NodeKey,'NodeData,'EdgeData>) : 'NodeKey * 'NodeKey * 'EdgeData =
                 Dictionary.item v1 g
                 |> fun (_, _, s) -> Dictionary.item v2 s
                 |> fun e -> (v1,v2,e)
     
         ///Lookup a labeled edge in the graph, returning a Some value if a binding exists and None if not.
-        let tryFind (nk1 : 'NodeKey) (nk2 : 'NodeKey) (g : FGraph<'NodeKey,'NodeData,'EdgeData>) : ('NodeKey * 'NodeKey * 'EdgeData) option =
+        static member tryFind (nk1 : 'NodeKey) (nk2 : 'NodeKey) (g : FGraph<'NodeKey,'NodeData,'EdgeData>) : ('NodeKey * 'NodeKey * 'EdgeData) option =
                 Dictionary.tryFind nk1 g
                 |> Option.bind (fun (_, _, s) -> Dictionary.tryFind nk2 s)
                 |> Option.map (fun e -> (nk1,nk2,e))
@@ -66,8 +66,8 @@ module FGraph =
         //Add and remove
 
         ///Adds a labeled, directed edge to the graph.
-        let tryAdd (nk1 : 'NodeKey) (nk2 : 'NodeKey) (ed : 'EdgeData) (g : FGraph<'NodeKey,'NodeData,'EdgeData>) : FGraph<'NodeKey,'NodeData,'EdgeData> option =
-            if (Nodes.contains nk1 g |> not) || (Nodes.contains nk2 g |> not) || contains nk1 nk2 g then
+        static member tryAdd (nk1 : 'NodeKey) (nk2 : 'NodeKey) (ed : 'EdgeData) (g : FGraph<'NodeKey,'NodeData,'EdgeData>) : FGraph<'NodeKey,'NodeData,'EdgeData> option =
+            if (Node.contains nk1 g |> not) || (Node.contains nk2 g |> not) || Edge.contains nk1 nk2 g then
                 None
             else 
                 let (p1, nd1, s1) = Dictionary.item nk1 g
@@ -77,7 +77,7 @@ module FGraph =
                 g |> Some
 
         ///Adds a labeled, directed edge to the graph.
-        let add (nk1 : 'NodeKey) (nk2 : 'NodeKey) (ed : 'EdgeData) (g : FGraph<'NodeKey,'NodeData,'EdgeData>) : FGraph<'NodeKey,'NodeData,'EdgeData> =
+        static member add (nk1 : 'NodeKey) (nk2 : 'NodeKey) (ed : 'EdgeData) (g : FGraph<'NodeKey,'NodeData,'EdgeData>) : FGraph<'NodeKey,'NodeData,'EdgeData> =
             //if Nodes.contains nk1 g |> not then failwithf "Source Node %O does not exist" nk1 
             //if Nodes.contains nk2 g |> not then failwithf "Target Node %O does not exist" nk2
             //if contains nk1 nk2 g then failwithf "An Edge between Source Node %O Target Node %O does already exist; FSharp.FGL does not allow for duplicate edges" nk1 nk2
