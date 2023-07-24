f6447c3775c64052fef5698a42e812ef1e84ff41
muehlhaus <timo.muehlhaus@rptu.de>
2023-07-24 17:48:29 +0200

    Add AdjGraph testing

--- tests/Graphoscope.Tests/AdjGraph.fs
+++ tests/Graphoscope.Tests/AdjGraph.fs
@@ -1,2 +1,20 @@
 ﻿module AdjGraph
 
+open System
+open System.Collections.Generic
+open Xunit
+open Graphoscope
+
+[<Fact>]
+let ``Can create empty graph and add nodes and edges`` () =
+    let emptyGraph = Builders.create<int, float>()
+    addNode emptyGraph 1
+    addNode emptyGraph 2
+    addNode emptyGraph 3
+    let edge = (1,3, 1.0)
+    addEdge emptyGraph edge
+   
+    Assert.Equal(1.0, (Measures.getVolume emptyGraph))
+    Assert.Equal(3.0, (Measures.getSize emptyGraph))
+    Assert.Equal(0.6666666666666666, (Measures.getMeanDegree emptyGraph))
+
