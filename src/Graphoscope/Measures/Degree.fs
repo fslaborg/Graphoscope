162613c454ad42d95b4da87688d4c55eea062ce2
muehlhaus <timo.muehlhaus@rptu.de>
2023-07-27 20:22:42 +0200

    Add measure degree testing

--- src/Graphoscope/Measures/Degree.fs
+++ src/Graphoscope/Measures/Degree.fs
@@ -5,12 +5,32 @@ open FSharpAux
 
 type Degree() =
 
-    static member inward(graph : FGraph<'NodeKey,'NodeData,'EdgeData>) = 42.
+    static member inwardDistributio(graph : FGraph<'NodeKey,'NodeData,'EdgeData>) = 42.
+    static member inwardDistributio(graph : DiGraph<'NodeKey, 'EdgeData>) = 42.
     
-    static member outward() = 42.
+
+    static member outwardDistribution(graph : FGraph<'NodeKey,'NodeData,'EdgeData>) = 42.
+    static member outwardDistribution(graph : DiGraph<'NodeKey, 'EdgeData>) = 42.
     
-    static member degree() = 42.
-    
+    /// <summary> 
+    /// Returns the degree distribution of the graph
+    /// </summary>
+    /// <param name="graph">The graph to be analysed</param> 
+    /// <returns>A float seq of degree values</returns>
+    static member distribution(graph : FGraph<'NodeKey,'NodeData,'EdgeData>) = 
+        graph
+        |> FGraph.mapContexts FContext.degree
+        |> Seq.map (fun (_,d) -> float d)
+
+    /// <summary> 
+    /// Returns the degree distribution of the graph
+    /// </summary>
+    /// <param name="graph">The graph to be analysed</param> 
+    /// <returns>A float seq of degree values</returns>
+    static member distribution(graph : DiGraph<'NodeKey, 'EdgeData>) = 
+        graph.OutEdges 
+        |> ResizeArray.map(fun n -> n |> ResizeArray.length |> float)
+        |> ResizeArray.toSeq    
     
     /// <summary> 
     /// Get the mean degree of the graph. 
@@ -18,7 +38,10 @@ type Degree() =
     /// </summary>
     /// <param name="graph">The graph to be analysed</param> 
     /// <returns>A float of the mean degree</returns>
-    static member mean(graph : FGraph<'NodeKey,'NodeData,'EdgeData>) = 42.
+    static member average(graph : FGraph<'NodeKey,'NodeData,'EdgeData>) = 
+        graph
+        |> FGraph.mapContexts FContext.degree
+        |> Seq.averageBy (fun (_,d) -> float d) 
     
     /// <summary> 
     /// Get the mean degree of the graph. 
@@ -26,11 +49,11 @@ type Degree() =
     /// </summary>
     /// <param name="graph">The graph to be analysed</param> 
     /// <returns>A float of the mean degree</returns>
-    static member mean(graph : DiGraph<'NodeKey, 'EdgeData>) =
+    static member average(graph : DiGraph<'NodeKey, 'EdgeData>) =
         graph.OutEdges
         |> ResizeArray.map(fun n -> (n |> ResizeArray.length) * 2 |> float)
         |> ResizeArray.toArray
         |> Array.average
 
 
-    static member compute() = 42.
+    //static member compute() = 42.
---
+++ tests/Graphoscope.Tests/Degree.fs
@@ -0,0 +1,28 @@
+﻿module Degree
+
+open System
+open Xunit
+open Graphoscope
+open Graphoscope.DiGraph
+open System.IO
+open FSharpAux
+
+[<Fact>]
+let ``Monkey graph import has correct measures`` () =
+    //measures taken from http://konect.cc/networks/moreno_rhesus/
+    let file = Path.Combine(Environment.CurrentDirectory, "ReferenceGraphs/out.moreno_rhesus_rhesus.txt")
+    
+     
+    let monkeyGraph = 
+        File.ReadLines file
+        |> Seq.skip 2 
+        |> Seq.map 
+            (fun str -> 
+                let arr = str.Split(' ')
+                int arr.[0], arr.[0], int arr.[1], arr.[1], float arr.[2])
+        |> FGraph.ofSeq
+    
+    
+    //Assert.Equal(111.0, (Measures.getVolume monkeyGraph))
+    //Assert.Equal(16, (Measures.getSize monkeyGraph)) 
+    Assert.Equal(13.8750,(Measures.Degree.average monkeyGraph)) 
