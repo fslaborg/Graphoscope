5bdf537b45a5f55636e4bb5e65c78e2ebe37dd31
muehlhaus <timo.muehlhaus@rptu.de>
2023-07-30 20:00:56 +0200

    Add BellmanFord algorithm on FGraph

---
+++ src/Graphoscope/Algorithms/BellmanFord.fs
@@ -0,0 +1,38 @@
+﻿namespace Graphoscope.Algorithms
+
+open Graphoscope
+open System.Collections.Generic
+
+
+/// <summary> 
+/// Computes BellmanFord shortest path
+/// </summary>
+type BellmanFord() =
+
+
+    static member ofFGraph (starting: 'NodeKey) (graph :  FGraph<'NodeKey, 'NodeData, float>) =
+        let nodesCount = graph.Count
+        let distances = new Dictionary<'NodeKey, float>()
+
+        // Step 1: Initialize distances from src to all
+        // other vertices as INFINITE
+        // Initialize distances to infinity for all nodes except the starting node
+        for nodeKey in graph.Keys do
+            if nodeKey = starting then
+                distances.[nodeKey] <- 0
+            else
+                distances.[nodeKey] <- System.Double.MaxValue
+ 
+        // Step 2: Relax all edges |V| - 1 times. A simple
+        // shortest path from src to any other vertex can
+        // have at-most |V| - 1 edges
+        for nkv in graph do
+            let (_,_,p) = nkv.Value 
+            for ekv in p do 
+                if (distances[nkv.Key] <> System.Double.MaxValue
+                    && distances[nkv.Key] + ekv.Value < distances[ekv.Key]) then
+                    distances[ekv.Key] <- distances[nkv.Key] + ekv.Value
+        
+        distances
+
+
---
+++ tests/Graphoscope.Tests/BellmanFord.fs
@@ -0,0 +1,37 @@
+﻿module BellmanFord
+
+open Xunit
+open Graphoscope
+open FSharpAux
+
+
+
+[<Fact>]
+let ``Dijkstra simple example on FGraph works correctly`` () =
+
+
+
+    let bfTest =
+        FGraph.empty
+        |> FGraph.addElement 0 "Node A" 1 "Node B" -1.
+        |> FGraph.addElement 0 "Node A" 2 "Node C" 4.
+        |> FGraph.addElement 1 "Node B" 2 "Node C" 3.
+        |> FGraph.addElement 1 "Node B" 3 "Node D" 2.
+        |> FGraph.addElement 1 "Node B" 4 "Node E" 2.
+        |> FGraph.addElement 3 "Node D" 2 "Node C" 5.
+        |> FGraph.addElement 3 "Node D" 1 "Node B" 1.
+        |> FGraph.addElement 4 "Node E" 3 "Node D" -3.
+
+    let actual = Algorithms.BellmanFord.ofFGraph 0 bfTest
+
+
+    // Vertex   Distance from Source
+    // 0          0
+    // 1          -1
+    // 2          2
+    // 3          -2
+    // 4          1
+
+    let expected = [(0, 0.0); (1, -1.); (2, 2.0); (3, -2.0); (4, 1.0)] |> Set.ofList 
+
+    Assert.Equal<Set<int*float>>(expected, actual |> Dictionary.toSeq |> Set.ofSeq)
