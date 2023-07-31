de9dad52593f987f8792925612d2b896c539b48f
Christopher Lux <lux-christopher@web.de>
2023-07-31 15:11:49 +0200

    Update Gilbert wo work for FGraph

--- src/Graphoscope/RandomModels/Gilbert.fs
+++ src/Graphoscope/RandomModels/Gilbert.fs
@@ -14,14 +14,44 @@ type Gilbert() =
     /// <summary> 
     /// Inits a directed AdjGraph according to the gilbert random plane networks
     /// </summary>
-    static member initDirectedAdjGraph (numberOfModes: int) (probability: float) =
+    static member initDirectedAdjGraph (numberOfNodes: int) (probability: float) =
          if probability > 1. || probability < 0. then failwithf "The stated probability %F is outside the expected range of 0. to 1." probability
 
          let rnd = new System.Random()
          let g   = AdjGraph.empty
 
-         for i=0 to (numberOfModes-1) do          
-             for ii=0 to (numberOfModes-1) do
+         for i=0 to (numberOfNodes-1) do          
+             for ii=0 to (numberOfNodes-1) do
                  if rnd.NextDouble() < probability then
                      g |> AdjGraph.addElement i i ii ii (i+ii)  |> ignore
          g
+
+    /// <summary> 
+    /// Inits a directed FGraph according to the gilbert random plane networks
+    /// </summary>
+    static member initDirectedFGraph (numberOfNodes: int) (probability: float) =
+        if probability > 1. || probability < 0. then failwithf "The stated probability %F is outside the expected range of 0. to 1." probability
+
+        let rnd = new System.Random()
+        let g   :  FGraph<int, int, float> = FGraph.empty
+
+        for i=0 to (numberOfNodes-1) do          
+            for ii=0 to (numberOfNodes-1) do
+                if rnd.NextDouble() < probability then
+                    g |> FGraph.addElement i i ii ii probability |> ignore
+        g
+
+     /// <summary> 
+    /// Inits an undirected FGraph according to the gilbert random plane networks
+    /// </summary>
+    static member initUndirectedFGraph (numberOfNodes: int) (probability: float) =
+        if probability > 1. || probability < 0. then failwithf "The stated probability %F is outside the expected range of 0. to 1." probability
+
+        let rnd = new System.Random()
+        let g   :  FGraph<int, int, float> = FGraph.empty
+
+        for i=0 to (numberOfNodes-1) do          
+            for ii=i to (numberOfNodes-1) do
+                if rnd.NextDouble() < probability then
+                    g |> FGraph.addElement i i ii ii probability |> ignore
+        g       
