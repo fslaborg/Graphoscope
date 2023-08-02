4651149d1c777416add6244f57ea3765dfe0a9c0
Christopher Lux <lux-christopher@web.de>
2023-08-02 15:02:29 +0200

    add addElement to DiGraph

--- src/Graphoscope/DiGraph.fs
+++ src/Graphoscope/DiGraph.fs
@@ -237,6 +237,15 @@ type DiGraph() =
         g
  
 
+    static member addElement (nk1 : 'NodeKey) (nd1 : 'NodeData) (nk2 : 'NodeKey) (nd2 : 'NodeData) (ed : 'EdgeData) (g : DiGraph<'NodeKey, 'EdgeData>) : DiGraph<'NodeKey, 'EdgeData> =
+        if not (g.IdMap.ContainsKey nk1) then
+            DiGraph.addNode nk1 g
+        if not (g.IdMap.ContainsKey nk2) then
+            DiGraph.addNode nk2 g
+        
+        DiGraph.addEdge (nk1,nk2,ed) g
+        g
+
 
     /// <summary> 
     /// Builds a graph from a list of nodes. 
@@ -283,35 +292,3 @@ type DiGraph() =
     static member countEdges (graph: DiGraph<'NodeKey, 'EdgeData>)  :int = 
         DiGraph.getAllEdges graph 
         |> Array.length 
-
-module Import =
-
-    let private importRows (fullpath: string) (delimiter: string) (headerRows: int) (weightsIncluded: bool)  = 
-        let rows  = CsvFile.Load(fullpath, delimiter, skipRows = headerRows, hasHeaders = false).Rows
-        rows
-        |> Seq.map (fun row -> int row[0], int row[1], if weightsIncluded then float row[2] else 1.0)
-        |> Seq.toArray
-    
-    /// <summary> 
-    /// Imports and builds a Directed graph from an edge list file
-    /// </summary>
-    /// <param name="fullpath">The path and file name to the file to be imported</param> 
-    /// <param name="delimiter">The delimter between the nodes in the file. Often a space or a tab which can be indicated with a \t </param> 
-    /// <param name="headerRows">The number of meta data rows at the start of the file. These often begin with a % and there is usually 2.</param> 
-    /// <param name="weightsIncluded">Specifies if  there is a third column with the edge weights. These will be treated as floats.</param> 
-    /// <returns>A Directed graph containing the nodes and edges specified in the file</returns>
-    let importDirectedGraph (fullpath: string) (delimiter: string) (headerRows: int) (weightsIncluded: bool) = 
-        importRows fullpath delimiter headerRows weightsIncluded
-        |> DiGraph.createFromEdges
-
-    /// <summary> 
-    /// Imports and builds a Undirected graph from an edge list file
-    /// </summary>
-    /// <param name="fullpath">The path and file name to the file to be imported</param> 
-    /// <param name="delimiter">The delimter between the nodes in the file. Often a space or a tab which can be indicated with a \t </param> 
-    /// <param name="headerRows">The number of meta data rows at the start of the file. These often begin with a % and there is usually 2.</param> 
-    /// <param name="weightsIncluded">Specifies if  there is a third column with the edge weights. These will be treated as floats.</param> 
-    /// <returns>A Undirected graph containing the nodes and edges specified in the file</returns>
-    let importUnDirectedGraph (fullpath: string) (delimiter: string) (headerRows: int) (weightsIncluded: bool) = 
-        importRows fullpath delimiter headerRows weightsIncluded
-        |> DiGraph.createFromEdges
