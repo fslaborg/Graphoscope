d4372bb8a08735d2174d46731a6a9bc0621aae6b
Christopher Lux <lux-christopher@web.de>
2023-08-02 12:28:03 +0200

    Update structure

--- src/Graphoscope/Measures/NetworkDensity.fs
+++ src/Graphoscope/Measures/NetworkDensity.fs
@@ -22,4 +22,38 @@ type GraphDensity() =
         let potentialConnections = ((nodesCount) * (nodesCount-1.)) / 2.
         let density = edgeCount / potentialConnections
         density
-        
+    
+    //TODO
+    /// <summary> 
+    /// Computes the graph density of the given graph <paramref name="graph"/>.
+    /// </summary>
+    /// <param name="graph">The graph for which to compute the graph density.</param>
+    /// <remarks> This calculation only works on graphs without self loops </remarks>
+    /// <returns>
+    /// The graph density.
+    /// </returns> 
+    static member ofDiGraph (graph: DiGraph<'NodeKey,'EdgeData>) = 
+        "TODO"  
+
+    /// <summary> 
+    /// Computes the graph density of the given graph <paramref name="graph"/>.
+    /// </summary>
+    /// <param name="graph">The graph for which to compute the graph density.</param>
+    /// <remarks> This calculation only works on graphs without self loops </remarks>
+    /// <returns>
+    /// The graph density.
+    /// </returns> 
+    static member compute (graph :  FGraph<'NodeKey, 'NodeData, float>) =
+        GraphDensity.ofFGraph graph
+    
+    /// <summary> 
+    /// Computes the graph density of the given graph <paramref name="graph"/>.
+    /// </summary>
+    /// <param name="graph">The graph for which to compute the graph density.</param>
+    /// <remarks> This calculation only works on graphs without self loops </remarks>
+    /// <returns>
+    /// The graph density.
+    /// </returns> 
+    static member compute (graph: DiGraph<'NodeKey,'EdgeData>) = 
+        GraphDensity.ofDiGraph graph
+    
