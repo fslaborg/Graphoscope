1fec5e4fab0473e6d05c565c853774633cb616d1
Christopher Lux <lux-christopher@web.de>
2023-09-11 17:24:40 +0200

    Update BetweennessCentrality.fs

--- src/Graphoscope/Measures/BetweennessCentrality.fs
+++ src/Graphoscope/Measures/BetweennessCentrality.fs
@@ -3,82 +3,71 @@ namespace Graphoscope.Measures
 open Graphoscope
 open System.Collections.Generic
 
+
+type ShortestPath={
+    target:int
+    path:Set<int>
+}
+
 type BetweennessCentrality() =
-    
-    static member getPathsForBetweennessOfFGraph (getEdgeWeight : 'EdgeData -> float) (graph :  FGraph<'NodeKey, 'NodeData, 'EdgeData> ) =
-        let ofUndirectedFGraphIncludingPath (starting : 'NodeKey) (toCheck: Set<'NodeKey>) (getEdgeWeight : 'EdgeData -> float) (pathDict:Dictionary<('NodeKey*'NodeKey),Set<'NodeKey>>) (graph :  FGraph<'NodeKey, 'NodeData, 'EdgeData> ) =
-            let getPathsOfDic key (dic:Dictionary<'NodeKey,('NodeKey*float)>) =
-                let getPrev key =
-                    dic.Item key |> fst
-                let rec getPath key (set:Set<'NodeKey>) =
-                    let prev = getPrev key
-                    if prev = starting then 
-                        set
-                    else
-                        let newSet = Set.add prev set
-                        getPath prev newSet
-                getPath key Set.empty
-                
-            let distance = Dictionary<'NodeKey,('NodeKey*float)>()
-            let priorityQueue = SortedSet<'NodeKey * float>(Comparer<'NodeKey * float>.Create(fun (_, d1) (_, d2) -> compare d1 d2))
-            let infinity = System.Double.MaxValue
+        
+    static member returnPaths (shortestPathAlgorithm:'NodeKey->('EdgeData -> float) ->FGraph<'NodeKey,'NodeData,'EdgeData> -> Dictionary<'NodeKey,'NodeKey * float>) (nodeIndexer:'NodeKey->int) (getEdgeWeight : 'EdgeData -> float) (graph: FGraph<'NodeKey,'NodeData,'EdgeData>) = 
 
-            // Initialize distances to infinity for all nodes except the starting node
-            // TODO: this can be improved by getOrDefault
-            for nodeKey in toCheck do
-                if nodeKey = starting then
-                    distance.[nodeKey] <- (starting,0.)
+        let paths :Dictionary<('NodeKey),Set<ShortestPath>> = Dictionary<('NodeKey),Set<ShortestPath>>()
+
+        let getPathsOfDic key (dic:Dictionary<'NodeKey,('NodeKey*float)>) starting=
+            let getPrev key =
+                dic.Item key |> fst
+            let rec getPath key (set:Set<'NodeKey>) =
+                let prev = getPrev key
+                //printfn $"Key:{key};prev:{prev}"
+                if prev = starting then 
+                    set
                 else
-                    distance.[nodeKey] <- (starting,infinity)
+                    let newSet = Set.add prev set
+                    getPath prev newSet
+            getPath key Set.empty
 
-            priorityQueue.Add((starting, 0)) |> ignore
 
-            while priorityQueue.Count > 0 do
-                let (currentNode, currentDistance) = priorityQueue.Min
-                priorityQueue.Remove(priorityQueue.Min) |> ignore
-            
-                let neighbours = graph.[currentNode] |> FContext.neighbours |> Seq.filter(fun (nk,ed) -> Set.contains nk toCheck)
-
-                for node,rawDistance in neighbours do
-                    let weightedDistance = rawDistance |> getEdgeWeight
-                    if weightedDistance < 0. then failwithf "Dijkstra does not handle neg. edge weigth"
-                    let totalDistance = (currentDistance + weightedDistance) // Assuming edgeWeight is always 1 in this example
-                    // Impove getValue
-                    let prevNode,prevDistance = distance.[node]
-                    if totalDistance < prevDistance then
-                        distance.[node] <- (currentNode,totalDistance)
-                        priorityQueue.Add((node, totalDistance)) |> ignore
-            
-            
-            for nkV in distance do
-                let key = nkV.Key 
-                let path = getPathsOfDic key distance
-                pathDict.Add((starting,key),path)
-
-            pathDict
-
-        let pathDict = Dictionary<('NodeKey*'NodeKey),Set<'NodeKey>>()
-
-        let rec getPaths (keysList:'NodeKey list) =
-            if keysList = List.empty then
-                pathDict
+        let rec buildSet (set:Set<ShortestPath>) (starting:'NodeKey) (sp: Dictionary<'NodeKey,('NodeKey * float)>) (keysList:'NodeKey list) counter =
+            if counter=keysList.Length then
+                set
             else
-                ofUndirectedFGraphIncludingPath keysList.Head (keysList|>Set.ofSeq) getEdgeWeight pathDict graph
-                getPaths keysList.Tail
-
-        getPaths (graph.Keys|>List.ofSeq)
-
-
-    static member getBetweennessOfFGraphNode (result:Dictionary<'NodeKey * 'NodeKey,Set<'NodeKey>>) (key:'NodeKey) =
-        let countPaths =
-            result.Keys.Count*2|>float
-        let countKeys =
-            let mutable count = 0.
-            for nkv in result do
-                if nkv.Value.Contains key then
-                    count <- count+2.
+                let x = keysList.[counter] 
+                if x=starting then 
+                    buildSet set starting sp keysList (counter+1)
                 else
-                    ()
-            count
-        countKeys / countPaths
-    
+                    let setToSave = {
+                        target=x
+                        path=getPathsOfDic x sp starting
+                    }
+                    let newSet = set.Add(setToSave)
+                    buildSet newSet starting sp keysList (counter+1)
+
+
+        let rec getPaths (keysList:'NodeKey list)  counter =
+            if counter = Seq.length keysList then
+                paths
+            else
+                let starting = keysList.[counter]
+                let sp = shortestPathAlgorithm (starting) getEdgeWeight graph
+                let set = buildSet Set.empty starting sp keysList 0
+
+                paths.Add(starting,set)
+                    
+                getPaths keysList (counter+1)
+        getPaths (graph.Keys|>Seq.map(fun x -> nodeIndexer x)|>List.ofSeq) 0
+
+    static member getBetweennessOfPathsAndNode (paths:Dictionary<int,Set<ShortestPath>>) (nodeIndexer:'NodeKey->int) (node:'NodeKey)=
+        let pathCount = paths.Count*(paths.Count-1)|>float
+        
+        paths
+        |> Seq.choose(fun nkv -> 
+            let nodeV = nodeIndexer node
+            if nkv.Key<>(nodeV) then
+                Some (nkv.Value|>Seq.sumBy(fun (x:ShortestPath) -> if x.target<>nodeV && x.path.Contains nodeV then 1. else 0.))
+            else    
+                None
+        )
+        |> Seq.sum
+        |> fun x -> x/pathCount
