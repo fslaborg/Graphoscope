35d47ea6a7fb4c9e0e604b41c2fefab66339c65e
Luis Ángel Méndez Gort <lamg@users.noreply.github.com>
2023-10-02 09:41:10 +0200

    refactoring `graph[key]` value
    Co-authored-by: Valber M. Silva de Souza <valbers@gmail.com>

--- src/Graphoscope/Algorithms/WedgeCount.fs
+++ src/Graphoscope/Algorithms/WedgeCount.fs
@@ -6,8 +6,9 @@ type WedgeCount() =
     static member ofGraph(graph: FGraph<'NodeKey, 'NodeData, 'EdgeData>) =
         graph.Keys
         |> Seq.sumBy (fun key ->
-            let successors = graph[key] |> FContext.successors |> Seq.map fst
-            let predecessors = graph[key] |> FContext.predecessors |> Seq.map fst
+            let node = graph[key]
+            let successors = node |> FContext.successors |> Seq.map fst
+            let predecessors = node |> FContext.predecessors |> Seq.map fst
             // exclude self loops and double edges
             let exclude = Seq.length (successors |> Seq.filter (fun x ->
                 let isDoubleEdge = predecessors |> Seq.contains x
