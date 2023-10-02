2a99bc9c237d55a0c4867f7fe9cb3fbe262bafa2
Luis Ángel Méndez Gort <lamg@users.noreply.github.com>
2023-10-02 09:38:24 +0200

    refactoring constant usage
    Co-authored-by: Valber M. Silva de Souza <valbers@gmail.com>

--- tests/Graphoscope.Tests/WedgeCount.fs
+++ tests/Graphoscope.Tests/WedgeCount.fs
@@ -14,16 +14,16 @@ let ``Wedge Count algorithm test graph without cycles`` () =
     // 1, 2 and 1, 8
     // 2, 5 and 2, 4
     // 2, 5 and 5, 7
-
+    let dummyEdgeData = 0.1
     let actual =
             FGraph.empty
-            |> FGraph.addElement 1 "Node 1" 2 "Node 2" 0.1
-            |> FGraph.addElement 1 "Node 1" 3 "Node 3" 0.1
-            |> FGraph.addElement 1 "Node 1" 8 "Node 8" 0.1
-            |> FGraph.addElement 2 "Node 2" 4 "Node 4" 0.1
-            |> FGraph.addElement 2 "Node 2" 5 "Node 5" 0.1
-            |> FGraph.addElement 3 "Node 3" 6 "Node 6" 0.1
-            |> FGraph.addElement 5 "Node 5" 7 "Node 7" 0.1
+            |> FGraph.addElement 1 "Node 1" 2 "Node 2" dummyEdgeData
+            |> FGraph.addElement 1 "Node 1" 3 "Node 3" dummyEdgeData
+            |> FGraph.addElement 1 "Node 1" 8 "Node 8" dummyEdgeData
+            |> FGraph.addElement 2 "Node 2" 4 "Node 4" dummyEdgeData
+            |> FGraph.addElement 2 "Node 2" 5 "Node 5" dummyEdgeData
+            |> FGraph.addElement 3 "Node 3" 6 "Node 6" dummyEdgeData
+            |> FGraph.addElement 5 "Node 5" 7 "Node 7" dummyEdgeData
             |> Algorithms.WedgeCount.ofGraph
     let expected = 8
     Assert.Equal(expected, actual)
