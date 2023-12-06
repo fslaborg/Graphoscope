c14106f2f98c4196959d603a5fa1283a887cee14
Christopher Lux <lux-christopher@web.de>
2023-12-06 11:24:37 +0100

    Add tests for LongestPath
    Add simple tests for LongestPath

---
+++ tests/Graphoscope.Tests/LongestPath.fs
@@ -0,0 +1,49 @@
+﻿module LongestPath
+
+open System
+open Xunit
+open Graphoscope
+open System.IO
+
+open Xunit
+
+[<Fact>]
+let ``smallCyclicGraphReturnsExceptedResults``() =
+    let cyclicGraphExample = 
+        seq{
+            "a","b",1.
+            "a","c",1.
+            "a","d",15.
+            "a","e",1.
+            "c","e",1.
+            "b","d",13.
+            "d","e",1.
+            "e","b",1.
+            "d","d",2.
+        }    
+        |> Seq.map(fun (s, t, w) -> (s, s, t, t, w))
+        |> FGraph.ofSeq
+
+    let ex = new System.Exception("The given FGraph is not a Directed Acyclic Graph!")
+
+    // Assert
+    Assert.Throws<Exception>(fun () -> Measures.LongestPath.compute("a", cyclicGraphExample) |> ignore)
+        |> fun thrown -> Assert.Equal(ex.Message, thrown.Message)
+
+[<Fact>]
+let smallAcyclicGraphReturnsExceptedResults () =
+    
+    let acyclicGraphExample = 
+        seq{
+            "A","B",2.
+            "A","D",1. 
+            "B","C",2.
+            "D","C",17.2
+            "A","C",18.
+
+        }    
+        |>Seq.map(fun (s,t,w) -> (s,s,t,t,w))
+        |>FGraph.ofSeq
+
+    Assert.Equal((["A"; "D"; "C"], 18.2),Measures.LongestPath.computeByEdgeData("A",acyclicGraphExample))
+    Assert.Equal((["A"; "B"; "C"], 2.),Measures.LongestPath.compute("A",acyclicGraphExample))
